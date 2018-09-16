aa7a88a63560ac3a3b78733dd398575b02ac49cf
kazimsanlav <kazimsanlav@gmail.com>
2018-09-16 15:49:40 +0300

    added selecters

--- KNN/sketch.js
+++ KNN/sketch.js
@@ -3,17 +3,26 @@
 let ratings;
 
 function preload(){
-	data = name.loadJSON("ratings.json",()=>{
+	data = loadJSON("ratings.json",()=>{
 		ratings = data.ratings;
 	});
 }
 
 function setup() {
-	// print(ratings);
-	
+	createCanvas(windowWidth, windowHeight);
+	background(0);
 	let sel = createSelect();
-	sel.position(10,10);
-	sel.option('kazim');
+	sel.position(width/2-40,10);
+	let sel2 = createSelect();
+	sel2.position(width/2-40,30);
+	// sel.option('kazim');
+
+	Object.keys(ratings).forEach(rating => {
+		sel.option(rating);
+		sel2.option(rating);
+	});
+
+	print(ratings);
 	
 
 
