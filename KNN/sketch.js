6d83afbb1041689b1329e5548ccbe62a189be727
kazimsanlav <kazimsanlav@gmail.com>
2018-09-16 22:10:19 +0300

    added sketch in div

--- KNN/sketch.js
+++ KNN/sketch.js
@@ -1,41 +1,77 @@
 //@ts-nocheck
 
-let ratings;
+let data;
 
-function preload() {
-	data = loadJSON("ratings.json", () => {
-		ratings = data.ratings;
-	});
-}
+// var myp5 = new p5( function( sketch ) {
 
-function setup() {
-	noCanvas();
-	let sel = createSelect();
-	sel.parent('selectors');
-	let sel2 = createSelect();
-	sel2.parent('selectors');
-	let but = createButton('push');
-	but.parent('button');
-	let p = createP('ads');
-	p.parent('paragraph');
+// 	var x = 100; 
+// 	var y = 100;
+
+// 	sketch.preload = function(){
+// 		data = loadJSON("iris.json");
+// 	}
+  
+// 	sketch.setup = function() {
+// 	  sketch.createCanvas(200, 200);
+// 	};
+  
+// 	sketch.draw = function() {
+// 	  sketch.background(0);
+// 	  sketch.fill(255);
+// 	  sketch.rect(x,y,50,50);
+// 	};
+//   });
+
+
+let plot;
+
+var s = function( sketch ) {
+
+	var x = 100; 
+	var y = 100;
+  
+	sketch.setup = function() {
+	  plot = sketch.createCanvas(sketch.windowWidth*0.6, sketch.windowWidth*0.6);
+	  plot.parent('p5sketch');
+	};
+  
+	sketch.draw = function() {
+	  sketch.background(0);
+	  sketch.fill(255);
+	  sketch.rect(x,y,50,50);
+	};
+  };
+
+var myp5 = new p5(s, document.getElementById('p5sketch'));
+
+
+// function preload() {
+// 	data = loadJSON("iris.json", () => {
+// 		// ratings = data.ratings;
+// 	});
+// }
+
+// function setup() {
+// 	plot = createCanvas(500, 500);
+// 	plot.background(255);
+// 	// plot = select("#plot");
+
+// 	text('123123', 0,100);
+// 	// ax1.parent(plot);
+// 	// ax1.position(10, 10);
 	
-	but.mouseClicked(() => {
-		p.html(sel.value() + ' ' + sel2.value());
-	});
-
-	
+		
 	
 
-	Object.keys(ratings).forEach(rating => {
-		sel.option(rating);
-		sel2.option(rating);
-	});
-
-	print(ratings);
-
-
-}
-
-// function draw() {
 
 // }
+
+// when press on button
+function onClick() {
+	let p = document.getElementById("p1");
+	let val1 = float( document.getElementById("sepalLength").value);
+	let val2 = float( document.getElementById("sepalWidth").value);
+	let val3 = float( document.getElementById("petalLength").value);
+	let val4 = float( document.getElementById("petalWidth").value);
+	p.innerHTML = (val1+val2+val3+val4);
+}
