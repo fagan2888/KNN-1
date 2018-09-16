0797388ac9f11b707de9ea19416927421e239b6a
kazimsanlav <kazimsanlav@gmail.com>
2018-09-17 00:43:48 +0300

    added iris plot

--- KNN/sketch.js
+++ KNN/sketch.js
@@ -1,46 +1,75 @@
 //@ts-nocheck
 
 let data;
-
-// var myp5 = new p5( function( sketch ) {
-
-// 	var x = 100; 
-// 	var y = 100;
-
-// 	sketch.preload = function(){
-// 		data = loadJSON("iris.json");
-// 	}
-  
-// 	sketch.setup = function() {
-// 	  sketch.createCanvas(200, 200);
-// 	};
-  
-// 	sketch.draw = function() {
-// 	  sketch.background(0);
-// 	  sketch.fill(255);
-// 	  sketch.rect(x,y,50,50);
-// 	};
-//   });
-
-
 let plot;
+let isdrawedpoints = false;
+let scl;
+let margin = 0.25;
 
-var s = function( sketch ) {
+var s = function (sketch) {
 
-	var x = 100; 
-	var y = 100;
-  
-	sketch.setup = function() {
-	  plot = sketch.createCanvas(sketch.windowWidth*0.6, sketch.windowWidth*0.6);
-	  plot.parent('p5sketch');
+	let wid = sketch.windowWidth * 0.6;
+	let hei = sketch.windowWidth * 0.6;
+	scl = 1 / 18 * sketch.windowWidth;
+
+	sketch.preload = function () {
+		data = sketch.loadJSON("iris.json");
+	}
+
+	sketch.setup = function () {
+		plot = sketch.createCanvas(wid, hei);
+		plot.parent('p5sketch');
+		sketch.textAlign(sketch.CENTER);
+		sketch.ellipseMode(sketch.CENTER);
+		sketch.background(210);
+		sketch.push();
+		sketch.translate(scl * margin, hei - scl * margin);
+		sketch.drawGrid();
+		sketch.pop();
+		sketch.drawPoints();//draws only once
+
+		sketch.text('sepalLength', wid / 2, wid - 13);
+
+		sketch.rotate(-sketch.HALF_PI);
+		sketch.text('sepalWidth', -wid / 2, 20);
+
 	};
-  
-	sketch.draw = function() {
-	  sketch.background(0);
-	  sketch.fill(255);
-	  sketch.rect(x,y,50,50);
+
+	sketch.draw = function () {
+
 	};
-  };
+
+	sketch.drawGrid = function () {
+		// sketch.textAlign(sketch.LEFT);
+		sketch.stroke(180);
+		sketch.fill(120);
+		for (let x = -10; x <= 10; x++) {
+			sketch.line(x * scl, -sketch.height, x * scl, sketch.height);
+			if (sketch.int(x) != 0) {
+				sketch.text(x, x * scl, margin * scl);
+			}
+		}
+		for (let x = -10; x <= 10; x++) {
+			sketch.line(-sketch.width, x * scl, sketch.width, x * scl);
+			sketch.text(-x, 0, x * scl + margin * scl);
+		}
+	}
+
+	sketch.drawPoints = function () {
+		sketch.push();
+		sketch.noStroke();
+		sketch.fill(100, 0, 0.100);
+		if (!isdrawedpoints) {
+
+			for (let index = 0; index < Object.keys(data).length; index++) {
+				const element = data[index];
+				sketch.ellipse(scl * (margin + element.sepalLength), hei - scl * (margin + element.sepalWidth), 5);
+			}
+		}
+		isdrawedpoints = true;
+		sketch.pop();
+	}
+};
 
 var myp5 = new p5(s, document.getElementById('p5sketch'));
 
@@ -59,9 +88,9 @@ var myp5 = new p5(s, document.getElementById('p5sketch'));
 // 	text('123123', 0,100);
 // 	// ax1.parent(plot);
 // 	// ax1.position(10, 10);
-	
-		
-	
+
+
+
 
 
 // }
@@ -69,9 +98,9 @@ var myp5 = new p5(s, document.getElementById('p5sketch'));
 // when press on button
 function onClick() {
 	let p = document.getElementById("p1");
-	let val1 = float( document.getElementById("sepalLength").value);
-	let val2 = float( document.getElementById("sepalWidth").value);
-	let val3 = float( document.getElementById("petalLength").value);
-	let val4 = float( document.getElementById("petalWidth").value);
-	p.innerHTML = (val1+val2+val3+val4);
+	let val1 = Number(document.getElementById("sepalLength").value);
+	let val2 = Number(document.getElementById("sepalWidth").value);
+	let val3 = Number(document.getElementById("petalLength").value);
+	let val4 = Number(document.getElementById("petalWidth").value);
+	p.innerHTML = (val1 + val2 + val3 + val4);
 }
