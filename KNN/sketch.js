6b51c661e87468479938b45587332098c889eb60
kazimsanlav <kazimsanlav@gmail.com>
2018-09-16 12:44:15 +0300

    prog

--- KNN/sketch.js
+++ KNN/sketch.js
@@ -1,7 +1,26 @@
+//@ts-nocheck
+
+let ratings;
+
+function preload(){
+	data = name.loadJSON("ratings.json",()=>{
+		ratings = data.ratings;
+	});
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	// print(ratings);
+	
+	let sel = createSelect();
+	sel.position(10,10);
+	sel.option('kazim');
+	
+
+
+	
+
 }
 
-function draw() {
+// function draw() {
 
-}
+// }
